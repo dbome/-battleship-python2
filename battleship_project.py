42d032e7cd57a4f4c462b7706885c1745779c129
Bome <bome.contact@gmail.com>
2018-11-26 09:26:12 +0100

    Update battleship_project.py
    Comment deleted line 24

--- battleship_project.py
+++ battleship_project.py
@@ -22,8 +22,7 @@ ship_col = random_col(board)
 print ship_row
 print ship_col
 
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
+# Everything from here go in a for loop!
 for turn in range(4):
   guess_row = int(raw_input("Guess Row: "))
   guess_col = int(raw_input("Guess Col: "))
